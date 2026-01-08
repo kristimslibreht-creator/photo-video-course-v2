2c81619cb434ce75e413b99f01c54d4518d2e3e7
kristimslibreht-creator <kristimslibreht@gmail.com>
2026-01-08 08:08:30 +0300

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,5 @@
-import { Nav } from "@/components/Nav";
-import { Footer } from "@/components/Footer";
+import { Nav } from "../../components/Nav";
+import { Footer } from "../../components/Footer";
 import { login } from "./actions";
 
 export default function LoginPage({ searchParams }: { searchParams?: { next?: string } }) {
