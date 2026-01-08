af577f7e502b8ca71419659c44d4d40715a2597a
kristimslibreht-creator <kristimslibreht@gmail.com>
2026-01-08 08:12:41 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import { Nav } from "../../../components/Nav";
-import { Footer } from "../../../components/Footer";
-import { COURSE_TITLE, lessons } from "../../../lib/course";
+import { Nav } from "../components/Nav";
+import { Footer } from "../components/Footer";
+import { COURSE_TITLE, lessons } from "../lib/course";
 
 export default function HomePage() {
   return (
